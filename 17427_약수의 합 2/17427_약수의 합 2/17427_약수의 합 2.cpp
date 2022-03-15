aa3037d45b2aa4adfda8e256822ce182a5af5a10
rlgus <rlgustjd@gmail.com>
2022-03-16 00:29:03 +0900

    17427_약수의 합 2 수정

--- 17427_약수의 합 2/17427_약수의 합 2/17427_약수의 합 2.cpp
+++ 17427_약수의 합 2/17427_약수의 합 2/17427_약수의 합 2.cpp
@@ -6,22 +6,12 @@ int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
-	int n;
-	cin >> n;
+	int t;
+	cin >> t;
 
-	int t = 1;
-	for (int i = 2; i <= n / 2; i++) {
-		if (n % i == 0)t += i;
-	}
-	if(n!=1)t += n;
-	int t3 = 1;
-	for (int j = 2; j < t; j++) {
-		int t2 = 1;
-		for (int i = 2; i <= j / 2; i++) {
-			if (j % i == 0)t2 += i;
-		}
-		if (j != 1)t2 += j;
-		t3 += t2;
+	long long t3 = 0;
+	for (int i = 1; i <= t; i++) {
+		t3 += i * (t / i);
 	}
 	cout << t3;
 }
