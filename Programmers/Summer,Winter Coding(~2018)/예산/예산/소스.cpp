5ec286fda70efa89ddba10e76f9d83ea07418169
Hyeonseong <76834414+KiKi-Daehaksaeng@users.noreply.github.com>
2022-09-13 11:00:31 +0900

    프로그래머스_예산

---
+++ Programmers/Summer,Winter Coding(~2018)/예산/예산/소스.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for (auto i : d) {
+        if (i <= budget) {
+            budget -= i;
+            answer++;
+        }
+        else break;
+    }
+    return answer;
+}
